0005bf78d3e655b4a4c5164b2ae10a6515100595
shivabalan1712 <shivabalan2002@gmail.com>
2025-08-06 12:12:12 +0530

    exploring spring

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Student {
+    public Student() {
+        System.out.println("Creating Student");
+    }
+    public void show() {
+        System.out.println("In show method");
+    }
+}
